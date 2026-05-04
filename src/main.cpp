03ff962ce0bc303c86510f965043090308404a2a
parip69 <parip69@users.noreply.github.com>
2026-05-04 20:25:25 +0200

    feat: aktualisiere Firmware-Version auf 1.0.14 und bereinige die Anzeige der Versionsnummer in der UI

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 //******************************************************
 //         Main of MatterMQTTBridge.
 // nur hier die start wert der version Aendern.OK=======
-// @version: 1.0.13 <br> Builddatum 20:22:53 04-05.2026
+// @version: 1.0.14 <br> Builddatum 20:25:14 04-05.2026
 //****************************************************
 
 #include <Arduino.h>
@@ -39,7 +39,7 @@
 #define TEST_OUTPUT_PIN_5 33
 
 // ======================= GLOBALS =======================
-const char* firmwareVersion = "1.0.13 <br> Builddatum 20:22:53 04-05.2026";
+const char* firmwareVersion = "1.0.14 <br> Builddatum 20:25:14 04-05.2026";
 AsyncWebServer webServer(80);
 SettingsManager settingsManager;
 
